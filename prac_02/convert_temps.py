2a02dff51acda92f8d093b46ac491de07bee2edd
mashrin6004-droid <mashrin6004@gmail.com>
2025-10-01 00:48:49 +1000

    Add files via upload

---
+++ prac_02/convert_temps.py
@@ -0,0 +1,38 @@
+
+
+import random
+
+
+def main():
+
+    # create_input_file(15)
+    input_file = open("temps_input.txt", "r")
+    output_file = open("temps_output.txt", "w")
+    for line in input_file:
+        result = convert_fahrenheit_to_celsius(float(line))
+        # result = convert_celsius_to_fahrenheit(float(line))
+        print(result, file=output_file)
+    input_file.close()
+    output_file.close()
+
+
+def create_input_file(quantity):
+
+    temperatures_file = open("temps_input.txt", "w")
+    for i in range(quantity):
+        temperature = random.uniform(-200, 200)
+        print(temperature, file=temperatures_file)
+    temperatures_file.close()
+
+
+def convert_celsius_to_fahrenheit(celsius):
+
+    return celsius * 9.0 / 5 + 32
+
+
+def convert_fahrenheit_to_celsius(fahrenheit):
+    """Convert fahrenheit to celsius."""
+    return 5 / 9 * (fahrenheit - 32)
+
+
+main()
